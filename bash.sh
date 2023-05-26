c6c3f10ae4a8ef7548b07dd3d9261e8d7b3d0a0c
Yevhenii <48496423+ZheZheDoshka@users.noreply.github.com>
2023-05-27 01:35:28 +0300

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ZheZheDoshka/cloud_scripts
+sudo yum install python3-pip
+pip3 install matplotlib
+pip3 install numpy
+pip3 install pandas
+pip3 install boto3
+sudo pip3 install jupyter notebook
